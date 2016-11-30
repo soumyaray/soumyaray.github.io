a865a8d4c3a55c749dddb4205e4a253e95297e0c
Soumya Ray <soumya.ray@gmail.com>
2016-11-30 10:23:03 +0800

    update google analytics snippet

--- javascripts/google_analytics.js
+++ javascripts/google_analytics.js
@@ -1,1 +1,5 @@
-!function(e,t,a,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=t.createElement(a),s=t.getElementsByTagName(a)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,"script","https://www.google-analytics.com/analytics.js","ga"),ga("create","UA-50430831-1","auto"),ga("send","pageview");
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-50430831-1', 'auto');
+ga('send', 'pageview');
+</script>
+<script async src='https://www.google-analytics.com/analytics.js'></script>
