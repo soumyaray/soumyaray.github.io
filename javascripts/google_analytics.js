6c40f2fa5312c7e47d6c6a0e218c81665eb2d944
Soumya Ray <soumya.ray@gmail.com>
2016-11-30 10:53:52 +0800

    inline google analytics script

--- javascripts/google_analytics.js
+++
@@ -1,5 +0,0 @@
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga('create', 'UA-50430831-1', 'auto');
-ga('send', 'pageview');
-</script>
-<script async src='https://www.google-analytics.com/analytics.js'></script>
